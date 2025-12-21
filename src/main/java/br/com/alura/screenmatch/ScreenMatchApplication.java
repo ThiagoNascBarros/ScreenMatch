f2351770946ef16c00537b051778ed8d67081427
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-12-20 21:50:51 -0300

    feat: Create project

---
+++ src/main/java/br/com/alura/screenmatch/ScreenMatchApplication.java
@@ -0,0 +1,25 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenMatchApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("ScreenMatchApplication started");
+    }
+
+}
