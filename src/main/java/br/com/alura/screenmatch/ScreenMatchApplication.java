27befdf0138ffcf48e9883f65ec7be59e0cc2fbd
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-12-23 15:00:50 -0300

    refactor: Calling of method of showMenu() in run
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

--- src/main/java/br/com/alura/screenmatch/ScreenMatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenMatchApplication.java
@@ -1,9 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.Domain.Episode;
-import br.com.alura.screenmatch.Domain.Serie;
-import br.com.alura.screenmatch.Service.ConsumerAPI;
-import br.com.alura.screenmatch.Service.ConvertData;
+import br.com.alura.screenmatch.Main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,18 +14,9 @@ public class ScreenMatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var address = "https://www.omdbapi.com/?t=gilmore+girls&apikey=abd3a916";
-        ConvertData cvtData = new ConvertData();
+        Main  main = new Main();
 
-        var response = ConsumerAPI.getDataOfAPI(address);
-        var dataSerie = cvtData.convertData(response, Serie.class);
-        System.out.println(dataSerie.toString());
-
-        address = "https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=1&apikey=abd3a916";
-        response = ConsumerAPI.getDataOfAPI(address);
-        var dataEpisode = cvtData.convertData(response, Episode.class);
-        System.out.println(dataEpisode.toString());
-
+        main.showMenu();
     }
 
 }
