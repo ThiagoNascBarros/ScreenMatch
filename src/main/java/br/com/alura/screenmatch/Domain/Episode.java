5ac6d1e1cb65f8ecedce942ba9c4d3e9a15f2eb3
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-12-23 14:55:41 -0300

    feat: Create class of domain for perform operations with Episode
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

--- src/main/java/br/com/alura/screenmatch/Domain/Episode.java
+++ src/main/java/br/com/alura/screenmatch/Domain/Episode.java
@@ -2,20 +2,61 @@ package br.com.alura.screenmatch.Domain;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record Episode(@JsonAlias("Title") String title,
-                      @JsonAlias("Episode") Integer number,
-                      @JsonAlias("imdbRating") String assessment,
-                      @JsonAlias("Released") String releaseDate)
-{
+public class Episode {
+    private Integer season;
+    private String title;
+    private Integer number;
+    private Double assessment;
+    private LocalDate releaseDate;
+    public Integer getSeason() {
+        return season;
+    }
+
+    public void setSeason(Integer season) {
+        this.season = season;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Double getAssessment() {
+        return assessment;
+    }
+
+    public void setAssessment(Double assessment) {
+        this.assessment = assessment;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
     @Override
     public String toString() {
-        return """
-               Title: %s
-               Number: %d
-               Rating: %s
-               Release date: %s
-               """.formatted(title, number, assessment, releaseDate);
+        return  "season=" + season +
+                ", title='" + title + '\'' +
+                ", number=" + number +
+                ", assessment=" + assessment +
+                ", releaseDate=" + releaseDate;
     }
 }
