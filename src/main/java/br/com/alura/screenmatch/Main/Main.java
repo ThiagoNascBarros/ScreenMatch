9567987ed6fc1c490702e7d78b79efc1d77952ff
ThiagoNascBarros <thiago171nasciment@gmail.com>
2026-01-02 15:12:34 -0300

    refactor: Config project to course of Java with Spring Data JPA
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

--- src/main/java/br/com/alura/screenmatch/Main/Main.java
+++ src/main/java/br/com/alura/screenmatch/Main/Main.java
@@ -21,90 +21,63 @@ public class Main {
     private ConvertData cvtData = new ConvertData();
 
     public void showMenu() {
-        System.out.print("Digite o nome da série para buscar uma Serie: ");
+        var menu = """
+                1 - Search series
+                2 - Search episodes
+               \s
+                0 - Exit                                \s
+               \s""";
 
-        var nameSerie = input.nextLine()
+        System.out.println(menu);
+        var option = input.nextInt();
+        input.nextLine();
+
+        switch (option) {
+            case 1:
+                searchSerieWeb();
+                break;
+            case 2:
+                searchEpPutSerie();
+                break;
+            case 0:
+                System.out.println("Exit...");
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
+    }
+
+
+    private Serie getDataSerie() {
+        System.out.println("Enter the series name to search: ");
+        var serieName = input.nextLine()
                 .replace(" ", "+")
                 .toLowerCase();
 
-        var response = consumer.getDataOfAPI(ADDRESS + nameSerie + APIKEY);
+        var response = consumer.getDataOfAPI(ADDRESS + serieName + APIKEY);
 
-        var dataSerie = cvtData.convertData(response, Serie.class);
-//        System.out.println(dataSerie.toString());
+        return cvtData.convertData(response, Serie.class);
+    }
 
+    private void searchSerieWeb() {
+        Serie dados = getDataSerie();
+        System.out.println(dados);
+    }
+
+    private void searchEpPutSerie() {
+        var dataSerie = getDataSerie();
         List<Seasons> seasons = new ArrayList<>();
+
         for (int i = 1; i <= dataSerie.totalSeasons(); i++) {
-            response = ConsumerAPI.getDataOfAPI(ADDRESS + nameSerie +
+            var json = ConsumerAPI.getDataOfAPI(ADDRESS + dataSerie.title()
+                    .replace(" ", "+")
+                    .toLowerCase() +
                     "&season=" + i + APIKEY);
 
-            seasons.add(cvtData.convertData(response, Seasons.class));
+            seasons.add(cvtData.convertData(json, Seasons.class));
         }
 
-//        seasons.forEach(System.out::println);
-
-//        System.out.println(ADDRESS + nameSerie + APIKEY);
-
-//        seasons.forEach(t -> { t.episodes().forEach(e -> {
-//                System.out.println(e.title());
-//            });
-//        });
-
-        List<DataEpisode> dataEpisodes = seasons.stream()
-                .flatMap(s -> s.episodes().stream())
-                .collect(Collectors.toList());
-
-//        System.out.println("\nTop ten episodes");
-//        dataEpisodes.stream()
-//                .filter(ep -> !ep.assessment().equalsIgnoreCase("N/A"))
-//                .peek(System.out::println)
-//                .sorted(Comparator.comparing(DataEpisode::assessment).reversed())
-//                .peek(e -> System.out.println("Ordenação" + e))
-//                .limit(10)
-//                .peek(e -> System.out.println("Limite de 10" + e))
-//                .map(e -> e.title().toUpperCase())
-//                .peek(e -> System.out.println("Transformando titles em maiúsculo" + e))
-//                .forEach(System.out::println);
-
-        List<Episode> episodes = seasons.stream()
-                .flatMap(s -> s.episodes().stream()
-                        .map(dte -> new Episode(s.number(), dte))
-                ).collect(Collectors.toList());
-
-        episodes.forEach(System.out::println);
-
-
-        System.out.print("\nSearch title: ");
-        var titleSearch = input.nextLine();
-        Optional<Episode> search = episodes.stream()
-                .filter(e -> e.getTitle().toUpperCase().contains(titleSearch.toUpperCase()))
-                .findFirst();
-
-        if (search.isPresent()) {
-            System.out.println("Episode " + search.get().getTitle());
-        } else {
-            System.out.println("Episode not found");
-        }
-
-//        System.out.println("A partir de que ano você desea ver os episódios? ");
-//        var year = input.nextInt();
-//        input.nextLine();
-//
-//        LocalDate dateSearch = LocalDate.of(year, 1, 1);
-//
-//        episodes.stream()
-//                .filter(e -> e.getReleaseDate() != null && e.getReleaseDate().isAfter(dateSearch))
-////                .filter(e -> e.getTitle().startsWith("T") && e.getTitle().contains("and"))
-//                .forEach(e -> {
-//                    System.out.println("\nSeason: " + e.getSeason() +
-//                            "\nEpisode: " + e.getTitle() +
-//                            "\nData de lançamento: " + e.getReleaseDate().format(DateTimeFormatter.ofPattern("dd 'de' MMMM 'de' yyyy")));
-//                });
-
-        Map<Integer, Double> assessmentPutSeason = episodes.stream()
-                .collect(Collectors.groupingBy(Episode::getSeason,
-                        Collectors.averagingDouble(Episode::getAssessment)));
-
-        assessmentPutSeason.forEach((i,as) -> System.out.printf("Assessment from the season %d: %.2f\n", i, as));
+        seasons.forEach(System.out::println);
     }
 
     private void learningLambdas() {
@@ -117,6 +90,5 @@ public class Main {
                 .map(String::toUpperCase)
                 .forEach(System.out::println);
     }
-
 
 }
