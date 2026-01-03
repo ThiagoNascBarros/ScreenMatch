59b17421ab4ab5bc3337382de244b5ab106ab7b8
ThiagoNascBarros <thiago171nasciment@gmail.com>
2026-01-03 13:45:05 -0300

    feat: Create toString to class of communication Episode
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

--- src/main/java/br/com/alura/screenmatch/communication/RecordEpisode.java
+++ src/main/java/br/com/alura/screenmatch/communication/RecordEpisode.java
@@ -2,10 +2,22 @@ package br.com.alura.screenmatch.communication;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.annotation.Nonnull;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record RecordEpisode(@JsonAlias("Title") String title,
                             @JsonAlias("Episode") Integer number,
                             @JsonAlias("imdbRating") String assessment,
                             @JsonAlias("Released") String releaseDate) {
+
+    @Override
+    @Nonnull
+    public String toString() {
+        return "Episode{" +
+                "title='" + title + '\'' +
+                ", number=" + number +
+                ", assessment='" + assessment + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                '}';
+    }
 }
